0fa48e10e14cf82b1cef03f48a2ea5741d99be73
Tej Raj Shrestha <helloteju96@gmail.com>
2016-03-27 19:33:30 +0545

    Imagane index link added with menu of its

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -57,7 +57,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-        	['label' => 'About', 'url' => ['/site/about']],
+        	['label' => 'Imagane', 'url' => ['/imagane/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
