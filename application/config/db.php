78ab4efa73adcbcce0cb4983d8ede0b9bfa70c8c
Tej Raj Shrestha <helloteju96@gmail.com>
2016-03-27 19:20:22 +0545

    configure db setting

--- application/config/db.php
+++ application/config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2-dailytips',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=85.10.205.173:3306;dbname=yii2_facebook_ap',
+    'username' => 'tejrajs',
+    'password' => 'meteju@99',
     'charset' => 'utf8',
+	'tablePrefix' => 'dalitips_',
 ];
